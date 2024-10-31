a76964f27862966780f41fae9740b0cd49fbebf3
Abhinav Arora <113025167+abhinavvarora@users.noreply.github.com>
2024-10-31 12:40:42 +0530

    Added a solution for conditionals-loops/17

---
+++ solutions/03-conditionals-loops/17-subtract-the-product-and-sum-of-digits-of-an-integer/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  
+  public static void main(String[] args) {
+    int n = 23094;
+    int rem;
+    int product = 1; 
+    int sum = 0;
+    while (n != 0) {
+        rem = n % 10;
+        product *= rem;
+        sum += rem;
+        n /= 10;
+    }
+    System.out.println(product - sum);
+  }
+}
